55f7dd706a2f974d0f86ecc5ae1ca943447974f7
Lucas Hübner <lucashubn@gmail.com>
2021-11-18 16:09:50 -0300

    implement local storage

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
+var arrayAddress = new Array();
+
+
 form.addEventListener('submit', function (event) {
     event.preventDefault();
-    console.log('click addTask button');
 
     let inputText = document.getElementById('inputText').value;
     addTaskItem(inputText);
@@ -8,19 +10,40 @@ form.addEventListener('submit', function (event) {
 })
 
 function addTaskItem(taskName) {
-    var ol = document.getElementById('taskItems');
-    var newLi = document.createElement('li');
+    let ol = document.getElementById('taskItems');
+    let newLi = document.createElement('li');
     newLi.appendChild(document.createTextNode(taskItems));
     ol.appendChild(newLi);
     newLi.innerText = taskName;
     newLi.className = "liTask";
+    newLi.id = taskName;
 
-    //localStorage.JSON
+    let _local_storage = localStorage.getItem('enderecos');
+    let jsonLocalStorage
+    if(_local_storage === null){
+        jsonLocalStorage = emptyStorage(taskName);
+    }
+    else{
+        jsonLocalStorage = addToStorage(_local_storage, taskName);      
+    }
+    localStorage.setItem('enderecos', jsonLocalStorage);
 
     addCheckbox(newLi);
-    addDeleteButton(newLi);
-
+    addDeleteButton(_local_storage, newLi);
 }
+
+function emptyStorage(storageValue){
+    let jsonAddress = '{"enderecos": ["' + storageValue + '"]}';        
+    let object = JSON.parse(jsonAddress);
+    return JSON.stringify(object);
+}
+
+function addToStorage(_local_storage, storageValue){
+    arrayAddress = JSON.parse(_local_storage);
+    arrayAddress['enderecos'].push(storageValue);
+    return JSON.stringify(arrayAddress);
+}
+
 
 function addCheckbox(taskLi) {
     var checkBox = document.createElement("input");
@@ -29,7 +52,7 @@ function addCheckbox(taskLi) {
 
     checkBox.onclick = function () {
         if(checkBox.checked){
-            taskLi.style = "text-decoration: line-through;";//TODO: not affect the delete button
+            taskLi.style = "text-decoration: line-through;";
         }
         else{
             taskLi.style = "text-decoration: none;";
@@ -37,17 +60,34 @@ function addCheckbox(taskLi) {
     }
 }
 
-function addDeleteButton(taskLi) {
-    var button = document.createElement('button');
+function addDeleteButton(_local_storage, taskLi) {
+    let button = document.createElement('button');
     button.innerText = "Excluir";
     taskLi.appendChild(button);
 
-    button.onclick = function () { deleteTask(this) };
+    button.onclick = function () { deleteTask(_local_storage, this) };
 }
 
-function deleteTask(elem) {
-    let confirm = window.confirm('Voce tem certeza que deseja excluir esta tarefa?');
-    if (confirm) {
-        elem.parentElement.remove();
-    }
+function deleteTask(_local_storage, elem) {
+    //let confirm = window.confirm('Voce tem certeza que deseja excluir esta tarefa?');
+    //if (confirm) {
+    //    elem.parentElement.remove();
+        
+    //} for testing only
+
+    //TODO: fazer function
+    //localStorage.removeItem(taskName);
+
+    _local_storage = localStorage.getItem('enderecos');
+    var object = JSON.parse(_local_storage);
+    arrayAddress = object.enderecos;
+
+    console.log(elem.parentElement.id)//
+    let arrayDel = arrayAddress.filter(value => value !== elem.parentElement.id);
+
+    object.enderecos = arrayDel;
+    _local_storage = JSON.stringify(object);
+    localStorage.setItem('enderecos', _local_storage);
+
+    elem.parentElement.remove();
 }
